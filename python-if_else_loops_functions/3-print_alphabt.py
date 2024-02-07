a177880ecb9751801ef2a47e4cbd6943dbc69474
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-07 18:05:23 -0500

    ädd a program that prints all possible different combinations of two digits.

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if chr(c) not in 'qe':
-        print(chr(c), end="")
+    if c != ord('e') and c != ord('q'):
+        print("{:c}".format(c), end="")
