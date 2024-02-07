7ad232be2cda5d763e02aeeaf8ee46688be7f8f4
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-07 17:43:39 -0500

    ädd a function that computes a to the power of b and return the value

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
+new_num = number
 
-print(f'Last digit of {number} is {last}',end=' ')
+if number < 0:
+    new_num *= (-1)
 
-if last != 0 and last < 6:
-    print('and is less than 6 and not 0\n')
-elif last > 5:
-    print('and is greater than 5\n')
-elif last == 0:
-    print('and is 0\n')
+last_digit = (new_num % 10)
+
+if number < 0:
+    last_digit *= (-1)
+
+print(f'Last digit of {number:d} is {last_digit}', end=' ')
+
+if last_digit > 5:
+    print('and is greater than 5')
+elif last_digit == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
