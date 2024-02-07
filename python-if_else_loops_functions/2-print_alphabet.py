58c6a4c7f3016d0de7b9e8b2640ec2c1740b07a6
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-07 17:54:34 -0500

    ädd a function that computes a to the power of b and return the value

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for value in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(value), end='')
+for c in range(97, 123):
+    print(chr(c), end="")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for value in range(ord('a'), ord('z') + 1):
-    if chr(value) != 'e' and chr(value) != 'q':
-        print('{:c}'.format(value), end='')
+for c in range(ord('a'), ord('z') + 1):
+    if chr(c) not in 'qe':
+        print(chr(c), end="")
