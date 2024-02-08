b95046257270fd45243cfbcb129c212ec88b4892
camilo <camiloa.fetecuam@ecci.edu.co>
2024-02-07 19:05:57 -0500

    A script that prints two concatenated strings

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+
+"""
+2-print.py
+Description: Python script that prints exactly "Programming is like building a multilingual puzzle",
+followed by a new line.
+"""
 print('"Programming is like building a multilingual puzzle')
