080feb89af22bc25c39b40446c4ccf9a0842a06c
camilo <camiloa.fetecuam@ecci.edu.co>
2024-02-07 19:11:56 -0500

    A script that prints exactly Programming is like building a multilingual puzzle

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-
-"""
-2-print.py
-Description: Python script that prints exactly "Programming is like building a multilingual puzzle",
-followed by a new line.
-"""
 print('"Programming is like building a multilingual puzzle')
