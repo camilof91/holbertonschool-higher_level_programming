fe22228a745f98413ebafb9b00cfb2f1fb9ede32
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-07 21:11:11 -0500

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,8 +6,7 @@ if __name__ == "__main__":
     result_add = add(a, b)
     result_sub = sub(a, b)
     result_mul = mul(a, b)
-    result_div = div(a, b)
-    
+    result_div = div(a, b)    
     print("{} + {} = {}".format(a, b, result_add))
     print("{} - {} = {}".format(a, b, result_sub))
     print("{} * {} = {}".format(a, b, result_mul))
