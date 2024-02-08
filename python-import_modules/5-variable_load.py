cc4e90430fba5994d77d63fde75685f5ad8ed246
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-07 21:23:50 -0500

    program that prints all the names defined by the compiled module hidden_4.pyc

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     # Ejecutar el archivo variable_load_5.py en un nuevo espacio de nombres
     namespace = {}
     with open("variable_load_5.py", "r") as file:
         exec(file.read(), namespace)
-
     # Imprimir el valor de la variable a
     print(namespace.get("a"))
