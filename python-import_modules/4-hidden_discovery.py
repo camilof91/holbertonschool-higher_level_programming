dd5a4ba167a18a8b12e0e4349ece40037a487c2f
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-07 21:31:54 -0500

    program that prints all the names defined by the compiled module hidden_4.pyc

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import dis
-    with open("hidden_4.pyc", "rb") as file:
-        code = file.read()
-        instructions = dis.get_instructions(code)
-        names = {instr.argval for instr in instructions if instr.opname == "LOAD_CONST"}
-        for name in sorted(names):
-            if not name.startswith("__"):
-                print(name)
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
+from variable_load_5 import a
+
 if __name__ == "__main__":
-    # Ejecutar el archivo variable_load_5.py en un nuevo espacio de nombres
-    namespace = {}
-    with open("variable_load_5.py", "r") as file:
-        exec(file.read(), namespace)
-    # Imprimir el valor de la variable a
-    print(namespace.get("a"))
+    print(a)
