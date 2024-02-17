da2994879882fe91347464db81d8a8b28d0a2e06
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-17 11:05:13 -0500

    a class Square that defines a square by: (based on 5-square.py)

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
+'''
+     A function that prints My name is <first name> <last name>
+'''
+
+
 def say_my_name(first_name, last_name=""):
     '''
-    Prints "My name is <first_name> <last_name>".
-
-    Args:
-        first_name (str): The first name.
-        last_name (str): The last name. Defaults to an empty string.
-
-    Raises:
-        TypeError: If first_name or last_name is not a string.
+        Args:
+            @first_name: The first name of the person(string).
+            @last_name: The last name of the person(stroing).
+        Raises:
+            TypeError: If the first_name and last_name are not strings.
     '''
-    # Check if first_name is a string
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
-
-    # Check if last_name is a string
-    if not isinstance(last_name, str):
+    elif not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
-
-    # Print the message with first_name and last_name
-    print("My name is {} {}".format(first_name, last_name))
-
-# Test the function with provided examples
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-
-# Test for TypeError exception
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
+    else:
+        print('My name is {} {}'.format(first_name, last_name))
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+'''
+    A function that prints a text with 2 new lines;
+        after each of these characters:
+            ".", "?" and ":"
+'''
 
 
 def text_indentation(text):
     '''
-    Prints a text with 2 new lines after each occurrence of '.', '?', and ':'.
-
-    Args:
-        text (str): The input text.
-
-    Raises:
-        TypeError: If text is not a string.
+        Args:
+            text: The text to print(string).
+        Raises:
+            TypeError: If the text is not a string.
     '''
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-
-    # Iterate over each character in the text
-    for char in text:
-        # Print the character
-        print(char, end='')
-
-        # If the character is '.', '?', or ':', print 2 new lines
-        if char in ('.', '?', ':'):
-            print("\n\n", end='')
+    else:
+        res = []
+        a = 0
+        text_lenght= len(text)
+        space_skip = True
+        is_end = False
+        is_delim = False
+        for i in range(text_lenght):
+            is_end = i == text_lenght - 1
+            is_delim = text[i] in '.?:'
+            if is_delim or is_end:
+                res.append(text[a: i + 1] + ('\n\n' * is_delim))
+                space_skip = True
+                a = i + 1
+            elif text[i] in ' \t\r\v' and space_skip:
+                a += 1
+            elif text[i] == '\n':
+                res.append(text[a: i + 1].rstrip() + '\n')
+                a += 1
+                space_skip = True
+            else:
+                if space_skip:
+                    a = i
+                space_skip = False
+        print(''.join(res), end='')
