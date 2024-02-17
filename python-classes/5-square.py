28a4cd8ceba5b8d7d4c8300e1ca4080c1fd2efbd
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-16 19:08:44 -0500

    class Square that defines a square by: (based on 4-square.py)

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 '''
     A class Square that defines a square by:
-    (based on 3-square.py)
+    (based on 4-square.py)
 '''
 
 
 class Square:
-    '''
-        Instantiation with optional size
-    '''
     def __init__(self, size=0):
+
+        '''
+            Initializing this square class
+
+            Args:
+                size: represnets the size of the square defined
+        '''
 
         if not isinstance(size, int):
             '''
-            Raise:
-                TypeError: if size is not int
+                Raises:
+                    TypeError: if size is not int
             '''
             raise TypeError('size must be an integer')
+
         if size < 0:
             '''
-                Raise:
-                    ValueError: if size is < 0
+                Raises:
+                    ValueError: if size < 0
             '''
             raise ValueError('size must be >= 0')
 
@@ -29,34 +34,42 @@ class Square:
     @property
     def size(self):
         '''
-            Retrieve size
+            Retrieves size of square
         '''
-
         return self.__size
 
     @size.setter
     def size(self, value):
-
         if not isinstance(value, int):
             '''
-                Raise:
+                Raises:
                     TypeError: if size is not int
             '''
             raise TypeError('size must be an integer')
-
         if value < 0:
             '''
-                Raise:
-                    ValueError: if size is < 0
+                Raises:
+                    ValueError: if size < 0
             '''
             raise ValueError('size must be >= 0')
-
         self.__size = value
 
     def area(self):
         '''
-            Area of the aquare
+            Calculate area of the square
+            Returns: The square of the size
         '''
-        Area = (self.__size * self.__size)
+        Area = self.__size * self.__size
+
         return Area
 
+    def my_print(self):
+        '''
+            print the square in #
+        '''
+
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            print("#" * self.__size)
