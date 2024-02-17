5a3ce4b8d8d5956dea3693ce00fe4567e7d4c1e3
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-17 11:17:34 -0500

    a class Square that defines a square by: (based on 5-square.py)

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-'''
-     A function that prints My name is <first name> <last name>
-'''
-
+"""Defines a name-printing function."""
 
 def say_my_name(first_name, last_name=""):
-    """Function with two parameters
-    print the string complete
+    """Print a name.
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
     """
-    if isinstance(first_name, str):
-        if isinstance(last_name, str):
-            print("My name is {} {}".format(first_name, last_name))
-        else:
-            raise TypeError("last_name must be a string")
-    else:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
