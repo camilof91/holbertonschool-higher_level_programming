b3e15afda58d6c22080451074f797678b3abd846
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 17:01:48 -0500

    Added untracked files

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
-'''
-    A function that prints a text with 2 new lines;
-        after each of these characters:
-            ".", "?" and ":"
-'''
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    '''
-        Prints the given text with two new
-        lines after each occurrence of '.', '?', or ':'.
-        Args:
-            text: The text to print(string).
-        Raises:
-            TypeError: If the text is not a string.
-    '''
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
     if not isinstance(text, str):
-        raise TypeError('text must be a string')
+        raise TypeError("text must be a string")
 
-    else:
-        res = []
-        a = 0
-        text_length = len(text)
-        space_skip = True
-        is_end = False
-        is_delim = False
-        for i in range(text_length):
-            is_end = i == text_length - 1
-            is_delim = text[i] in '.?:'
-            if is_delim or is_end:
-                res.append(text[a: i + 1] + ('\n\n' * is_delim))
-                space_skip = True
-                a = i + 1
-            elif text[i] in ' \t\r\v' and space_skip:
-                a += 1
-            elif text[i] == '\n':
-                res.append(text[a: i + 1].rstrip() + '\n')
-                a += 1
-                space_skip = True
-            else:
-                if space_skip:
-                    a = i
-                space_skip = False
-        return ''.join(res)
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
