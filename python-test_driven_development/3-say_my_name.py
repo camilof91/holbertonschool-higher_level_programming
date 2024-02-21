d170bc40b4adbcf15c8400460b47a47c89f97a1d
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 11:09:39 -0500

    modify the last space

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Defines a name-printing function.
+"""
+
 def say_my_name(first_name, last_name=""):
     """Print a name.
 
@@ -8,11 +12,8 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If either of first_name or last_name are not strings.
     """
-    if not isinstance(first_name, str):
+     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        print("My name is {} ".format(first_name))
+    print("My name is {} {}".format(first_name, last_name))
