9e046f3c7a54bb171f5bc022b8f28eda63aa87c0
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 14:00:10 -0500

    Added untracked files

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,22 +7,37 @@
 
 
 def text_indentation(text):
-    """
-    Prints a text with 2 new lines after each of these characters: ., ? and :
-    """
+    '''
+        Args:
+            text: The text to print(string).
+        Raises:
+            TypeError: If the text is not a string.
+    '''
     if not isinstance(text, str):
-        raise TypeError("text must be a string")
+        raise TypeError('text must be a string')
 
-    punctuations = ['.', '?', ':']
-    lines = []
-    line = ''
-
-    for char in text:
-        line += char
-        if char in punctuations:
-            lines.append(line.strip() + '\n\n')
-            line = ''
-    if line.strip():
-        lines.append(line.strip())
-
-    print('\n'.join(lines))
+    else:
+        res = []
+        a = 0
+        text_lenght= len(text)
+        space_skip = True
+        is_end = False
+        is_delim = False
+        for i in range(text_lenght):
+            is_end = i == text_lenght - 1
+            is_delim = text[i] in '.?:'
+            if is_delim or is_end:
+                res.append(text[a: i + 1] + ('\n\n' * is_delim))
+                space_skip = True
+                a = i + 1
+            elif text[i] in ' \t\r\v' and space_skip:
+                a += 1
+            elif text[i] == '\n':
+                res.append(text[a: i + 1].rstrip() + '\n')
+                a += 1
+                space_skip = True
+            else:
+                if space_skip:
+                    a = i
+                space_skip = False
+        print(''.join(res), end='')
