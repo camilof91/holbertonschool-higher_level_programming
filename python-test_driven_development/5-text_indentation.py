3bafa654437171dd44a37744d7f4c067df4dad30
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 13:32:44 -0500

    Added untracked files

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,18 +10,21 @@ def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
     """
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    listSearch = [".", "?", ":"]
-    textIndent = ""
-    prev_char = ''
-    for char in text:
-        if char == " " and prev_char == " ":
-            continue
-        textIndent += char
-        if char in listSearch:
-            textIndent += "\n\n"
-        prev_char = char
-
-    print(textIndent, end="")
+    def text_indentation(text):
+        """
+        Prints a text with 2 new lines after each of these characters: ., ? and :
+        """
+        if not isinstance(text, str):
+            raise TypeError("text must be a string")
+        special_chars = [".", "?", ":"]
+        result = ""
+        line = ""
+        for char in text:
+            if char in special_chars:
+                line += char
+                result += line.strip() + "\n\n"
+                line = ""
+            else:
+                line += char
+        result += line.strip()
+        print(result)
