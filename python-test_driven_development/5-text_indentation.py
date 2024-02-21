42c777139db6d3b9f984f1ebd9c2846fc4328185
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 12:57:40 -0500

    modify the last space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,18 +15,16 @@ def text_indentation(text):
         TypeError: If size is not an integer.
         ValueError: If size is < 0
     """
-    if not isinstance(text, str):
+     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    
+
     res = []
     line = ""
     for char in text:
         line += char
-        if char in ".?:":
-            # Check if the next character is a space:
-            if len(text) > 1 and text[text.index(char) + 1].isspace():
-                res.append(line.strip())
-                res.append("\n\n")
-                line = ""
-    res.append(line.strip())  # Add the remaining text
+        if char in ".?:":  # Verifica si el carácter es un delimitador
+            res.append(line.rstrip())  # Agrega la línea actual sin espacios en blanco al final
+            res.append("\n\n")  # Agrega dos nuevas líneas después del delimitador
+            line = ""  # Restablece la línea para la próxima iteración
+    res.append(line.rstrip())  # Agrega la última línea sin espacios en blanco al final
     print("".join(res), end='')
