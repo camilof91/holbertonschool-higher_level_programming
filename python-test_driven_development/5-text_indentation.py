5edbceee4e94324d470b10eb6f379223a09d4caa
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 12:39:11 -0500

    modify the last space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,38 +5,32 @@
             ".", "?" and ":"
 '''
 
-
 def text_indentation(text):
-    '''
+    """
+    Print a text with 2 new lines after each '.', '?', and ':'
+
     Args:
-    text: The text to print(string).
+        text (str): The text to print.
+
     Raises:
-    TypeError: If the text is not a string.
-    '''
+        TypeError: If text is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    else:
-        res = []
-        a = 0
-        text_lenght = len(text)
-        space_skip = True
-        is_end = False
-        is_delim = False
-        for i in range(text_lenght):
-            is_end = i == text_lenght - 1
-            is_delim = text[i] in '.?:'
-            if is_delim or is_end:
-                res.append(text[a: i + 1] + ('\n\n' * is_delim))
-                space_skip = True
-                a = i + 1
-            elif text[i] in ' \t\r\v' and space_skip:
-                a += 1
-            elif text[i] == '\n':
-                res.append(text[a: i + 1].rstrip() + '\n')
-                a += 1
-                space_skip = True
-            else:
-                if space_skip:
-                    a = i
-                space_skip = False
-        print(''.join(res), end='')
+
+    result = ''
+    newline = True
+
+    for char in text:
+        if char == '.' or char == '?' or char == ':':
+            result += char + '\n\n'
+            newline = True
+        elif char != ' ' and char != '\t' and char != '\n' and newline:
+            result += char
+            newline = False
+        elif char != ' ' and char != '\t' and char != '\n' and not newline:
+            result += ' ' + char
+        elif char == ' ' and not newline:
+            result += char
+
+    print(result, end='')
