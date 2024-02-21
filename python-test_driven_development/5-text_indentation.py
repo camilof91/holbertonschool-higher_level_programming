428d28f1d429e4a5289236214611280251e90e7d
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 13:02:21 -0500

    modify the last space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,14 +7,7 @@
 
 
 def text_indentation(text):
-    """Print a square with the # character.
-
-    Args:
-        text (str): The height/width of the square.
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is < 0
-    """
+    """Prints a text with 2 new lines after each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError('text must be a string')
 
@@ -22,9 +15,9 @@ def text_indentation(text):
     line = ""
     for char in text:
         line += char
-        if char in ".?:":  # Verifica si el carácter es un delimitador
-            res.append(line.rstrip())  # Agrega la línea actual sin espacios en blanco al final
-            res.append("\n\n")  # Agrega dos nuevas líneas después del delimitador
-            line = ""  # Restablece la línea para la próxima iteración
-    res.append(line.rstrip())  # Agrega la última línea sin espacios en blanco al final
+        if char in ".?:":  # Check if the character is a delimiter
+            res.append(line.rstrip())  # Add the current line without trailing whitespace
+            res.append("\n\n")  # Add two new lines after the delimiter
+            line = ""  # Reset the line for the next iteration
+    res.append(line.rstrip())  # Add the last line without trailing whitespace
     print("".join(res), end='')
