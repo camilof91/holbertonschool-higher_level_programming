0f61c6e094225efd161f255b8e4e2994102b5f97
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 12:43:50 -0500

    modify the last space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,32 +5,24 @@
             ".", "?" and ":"
 '''
 
+
 def text_indentation(text):
-    """
-    Print a text with 2 new lines after each '.', '?', and ':'
-
+    '''
     Args:
-        text (str): The text to print.
-
+    text: The text to print(string).
     Raises:
-        TypeError: If text is not a string.
-    """
+    TypeError: If the text is not a string.
+    '''
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-
-    result = ''
-    newline = True
-
-    for char in text:
-        if char == '.' or char == '?' or char == ':':
-            result += char + '\n\n'
-            newline = True
-        elif char != ' ' and char != '\t' and char != '\n' and newline:
-            result += char
-            newline = False
-        elif char != ' ' and char != '\t' and char != '\n' and not newline:
-            result += ' ' + char
-        elif char == ' ' and not newline:
-            result += char
-
-    return result
+    else:
+        res = []
+        line = ""
+        for char in text:
+            line += char
+            if char in ".?:":
+                res.append(line.strip())
+                res.append("\n\n")
+                line = ""
+        res.append(line.strip())  # Add the remaining text
+        print("".join(res), end='')
