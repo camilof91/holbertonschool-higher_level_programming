ca16f1bda60027d010c876f89a38b2aaae32197b
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-21 13:06:38 -0500

    modify the last space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,10 @@
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """
+    Prints a text with 2 new lines 
+    after each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError('text must be a string')
 
