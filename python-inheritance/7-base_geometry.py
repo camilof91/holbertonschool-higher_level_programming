e5c3cb99b43fd8917567fcb7316f7455625e1282
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-25 18:38:12 -0500

    a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py)

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+'''This defines a base geometry class BaseGeometry.'''
 
 
 class BaseGeometry:
-    """Reprsent base geometry."""
+    '''Represent base geometry.'''
 
     def area(self):
-        """Not yet implemented."""
-        raise Exception("area() is not implemented yet")
+        '''Not implemented yet.'''
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
+        '''
+            Validate a parameter as an integer.
+            Args:
+                @name: The name (str) of the parameter.
+                @value: The parameter (integer) to validate.
+            Raises:
+                TypeError: If value is not an integer.
+                ValueError: If value is <= 0.
+        '''
 
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError('{} must be greater than 0'.format(name))
