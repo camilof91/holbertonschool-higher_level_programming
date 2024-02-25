a0dea0faf65e8c1981d306b98e9634d1c72c8e25
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-25 18:43:06 -0500

    a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py)

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,7 +3,8 @@
 
 
 def inherits_from(obj, a_class):
-    """Checks if an object is an inherited instance of a class.
+    """
+    Checks if an object is an inherited instance of a class.
 
     Args:
         obj (any): The object to check.
