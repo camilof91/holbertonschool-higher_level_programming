cfb4c487ea1f5325616a9e34da2d31abba68ffba
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-25 18:32:34 -0500

    a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py)

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,39 +1,10 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""Defines an inherited list class MyList."""
 
 
-class Square:
-    """Represents a square."""
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
 
-    def __init__(self, size):
-        """Initializes a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
-
-    @property
-    def size(self):
-        """Gets/sets the current size of the square."""
-        return (self.__size)
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """Returns the current area of the square."""
-        return (self.__size * self.__size)
-
-    def my_print(self):
-        """Prints the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
-        if self.__size == 0:
-            print("")
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
