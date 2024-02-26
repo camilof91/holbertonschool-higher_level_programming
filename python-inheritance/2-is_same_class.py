8faa7b7bab194dcc137c762c4da2c3fcd625dfea
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-26 12:30:05 -0500

    a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -13,6 +13,4 @@ def is_same_class(obj, a_class):
     If obj is exactly an instance of a_class - True.
     Otherwise - False.
     """
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return isinstance(obj, a_class)
