f38111a9466d39fb58c05bb1b2de008790894501
camilofetecua1 <camilofetecua2011@outlook.com>
2024-02-27 08:02:24 -0500

    first class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id = None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+if __name__ == "__main__":
+    # Ejemplo de uso
+    obj1 = Base()
+    print(obj1.id)  # Salida: 1
+
+    obj2 = Base()
+    print(obj2.id)  # Salida: 2
+
+    obj3 = Base(10)
+    print(obj3.id) 
+
+    obj4 = Base()
+    print(obj4.id)
+
+     
