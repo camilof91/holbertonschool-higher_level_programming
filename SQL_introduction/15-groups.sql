3dee0fa081136578b7a2c154f7407203aa9920ab
camilof91 <camilofetecua2011@outlook.com>
2024-04-01 12:59:44 -0500

    added commits

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,6 @@
- -- selects the "score" column and counts the occurrences of each distinct score value in the
+-- This query retrieves the score of each record in the "second_table" table
+-- and counts how many times each score appears in the table.
+-- Then, it groups the results by score using the GROUP BY clause.
+-- After that, it sorts the results in descending order based on the number of records
+-- with the same score using the ORDER BY clause.
 SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
