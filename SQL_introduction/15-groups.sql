72f35bb753d42178ab08d9d00f58b766bddf4a72
camilof91 <camilofetecua2011@outlook.com>
2024-04-01 12:49:07 -0500

    added commits

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -2,4 +2,4 @@
  -- "second_table" table. It uses the COUNT() function to count the occurrences and aliases
  -- the result as "number". The GROUP BY clause is used to group the results by the "score" 
  -- column, and the ORDER BY clause sorts the results in descending order based on the "score" column.
-SELECT `score`, COUNT(*) AS `number` FROM `second_table` GROUP BY `score` ORDER BY `number` DESC;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
