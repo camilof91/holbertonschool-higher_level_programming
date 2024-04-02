6181103ca31f2966337cfe661f93a491336aa01c
camilof91 <camilofetecua2011@outlook.com>
2024-04-02 17:54:53 -0500

    added creating user with all privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- This query shows the grants assigned to the user 'user_0d_1' when connecting from 'localhost'.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
 -- This query shows the grants assigned to the user 'user_0d_2' when connecting from 'localhost'.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
