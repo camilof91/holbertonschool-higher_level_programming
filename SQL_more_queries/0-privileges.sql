f17d9e5b26f4114656162e5140ae811aa2dbfb46
Camilo <152035075+camilof91@users.noreply.github.com>
2024-04-02 18:41:28 -0500

    Update 0-privileges.sql
    Change coments

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- This query shows the grants assigned to the user 'user_0d_1' when connecting from 'localhost'.
--- This query shows the grants assigned to the user 'user_0d_2' when connecting from 'localhost'.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- This query shows the grants assigned to the user 'user_0d_2' when connecting from 'localhost'.
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
