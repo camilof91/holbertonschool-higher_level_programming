bea763b3bcc9b449b2ecb0e99d3ff6bdb240e862
camilof91 <camilofetecua2011@outlook.com>
2024-04-03 18:48:50 -0500

    added files

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Create a database 'hbtn_0d_2' if it doesn't already exist.
--- Create a user 'user_0d_2' identified by the password 'user_0d_2_pwd' if the user doesn't already exist.
--- Grant SELECT privilege on all tables within the 'hbtn_0d_2' database to the user 'user_0d_2' when connecting from 'localhost'.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create a user 'user_0d_2' identified by the password 'user_0d_2_pwd' if the user doesn't already exist.
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'; 
+-- Grant SELECT privilege on all tables within the 'hbtn_0d_2' database to the user 'user_0d_2' when connecting from 'localhost'.
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
